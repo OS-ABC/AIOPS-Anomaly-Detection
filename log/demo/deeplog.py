67334e05fa0178ca5049a04df45d750b6ee41ef5
XuHui <44027187+Nono17@users.noreply.github.com>
2021-04-04 15:31:38 +0800

    修改配置文件

--- log/demo/deeplog.py
+++ log/demo/deeplog.py
@@ -3,6 +3,7 @@
 
 import argparse
 import sys
+import config
 sys.path.append('../')
 
 from logdeep.models.lstm import deeplog, loganomaly, robustlog
@@ -12,49 +13,11 @@ from logdeep.tools.utils import *
 
 
 # Config Parameters
-
-options = dict()
-options['data_dir'] = '../data/'
-options['window_size'] = 10
-options['device'] = "cpu"
-
-# Smaple
-options['sample'] = "sliding_window"
-options['window_size'] = 10  # if fix_window
-
-# Features
-options['sequentials'] = True
-options['quantitatives'] = False
-options['semantics'] = False
-options['feature_num'] = sum(
-    [options['sequentials'], options['quantitatives'], options['semantics']])
-
-# Model
-options['input_size'] = 1
-options['hidden_size'] = 64
-options['num_layers'] = 2
-options['num_classes'] = 28
-
-# Train
-options['batch_size'] = 2048
-options['accumulation_step'] = 1
-
-options['optimizer'] = 'adam'
-options['lr'] = 0.001
-options['max_epoch'] = 370
-options['lr_step'] = (300, 350)
-options['lr_decay_ratio'] = 0.1
-
-options['resume_path'] = None
-options['model_name'] = "deeplog"
-options['save_dir'] = "../result/deeplog/"
-
-# Predict
-options['model_path'] = "../result/deeplog/deeplog_last.pth"
-options['num_candidates'] = 9
+# config.py
 
 seed_everything(seed=1234)
 
+options = config.parse_args()
 
 def train():
     Model = deeplog(input_size=options['input_size'],
--- log/demo/robustlog.py
+++ log/demo/robustlog.py
@@ -3,6 +3,7 @@
 
 import argparse
 import sys
+import config
 sys.path.append('../')
 
 from logdeep.models.lstm import deeplog, loganomaly, robustlog
@@ -11,48 +12,7 @@ from logdeep.tools.train import Trainer
 from logdeep.tools.utils import *
 
 
-# Config Parameters
-
-options = dict()
-options['data_dir'] = '../data/'
-options['window_size'] = 10
-options['device'] = "cpu"
-
-# Smaple
-options['sample'] = "session_window"
-options['window_size'] = -1
-
-# Features
-options['sequentials'] = False
-options['quantitatives'] = False
-options['semantics'] = True
-options['feature_num'] = sum(
-    [options['sequentials'], options['quantitatives'], options['semantics']])
-
-# Model
-options['input_size'] = 300
-options['hidden_size'] = 128
-options['num_layers'] = 2
-options['num_classes'] = 2
-
-# Train
-options['batch_size'] = 256
-options['accumulation_step'] = 1
-
-options['optimizer'] = 'adam'
-options['lr'] = 0.001
-options['max_epoch'] = 60
-options['lr_step'] = (40, 50)
-options['lr_decay_ratio'] = 0.1
-
-options['resume_path'] = None
-options['model_name'] = "robustlog"
-options['save_dir'] = "../result/robustlog/"
-
-# Predict
-options['model_path'] = "../result/robustlog/robustlog_last.pth"
-options['num_candidates'] = -1
-
+options = config.parse_args()
 seed_everything(seed=1234)
 
 
